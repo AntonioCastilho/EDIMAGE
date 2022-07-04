4505210a59319c8d029ed9dd947d6abb744dd0a0
Antonio Castilho <castilhomec@gmail.com>
2022-07-03 21:59:12 -0300

    Etapa 2

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-from editor import ed 
+from editor import ed
 from kivy.app import App
 from kivy.lang.builder import Builder
+from kivy.core.window import Window
+
+Window.clearcolor = .92, .92, .92, 1
 
 bu = Builder.load_file('kvlang.kv')
 
+
 class Programa(App):
     title = 'Check-in'
+
     def build(self):
         return bu
 
